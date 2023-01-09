350adee419aaf843a7896522b42635bfd7bbc1bf
hardcommitoneself <hardcommitoneself@gmail.com>
2023-01-08 18:32:01 -0800

    init commit

---
+++ src/main/java/com/example/starter/StarterApplication.java
@@ -0,0 +1,20 @@
+package com.example.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@RestController
+public class StarterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
